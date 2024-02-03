3c1d3de89c95902c4d1835354339b5dc279371db
shawn <shawnbutton@gmail.com>
2024-02-03 17:10:32 -0500

    rename code and tests

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -3,108 +3,111 @@ import pytest
 from gilded_rose_python.gilded_rose import GildedRose
 from gilded_rose_python.item import Item
 
-gilded_rose = GildedRose()
+
+def makeItemList(name, sell_in, quality):
+    return [Item(name, sell_in, quality)]
 
 
 class TestGildedRose:
 
-    def test_regular_items_decrease_by_one(self):
-        items = [Item("+5 Dexterity Vest", 10, 20)]
-        gilded_rose.update_quality(items)
-        assert items[0].quality == 19
-        assert items[0].sell_in == 9
+    def test_should_degrade_quality_each_day(self):
+        items = makeItemList("foo", 6, 5)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 4
 
-    def test_quality_goes_up_for_improving_products(self):
-        self.items.append(Item("Aged Brie", 20, 30))
-        self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 20, 30))
-        gilded_rose.update_quality(self.items)
-        expected = [
-            {'sell_in': 19, 'quality': 31},
-            {'sell_in': 19, 'quality': 31},
-        ]
+    def test_should_degrade_quality_twice_as_fast_after_sell_by_date(self):
+        items = makeItemList("foo", 1, 5)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 4
+        gilded_rose.update_quality()
+        assert items[0].quality == 2
 
-        for index, expectation in enumerate(expected):
-            item = self.items[index]
-            assert item.quality == expectation['quality']
-            assert item.sell_in == expectation['sell_in']
+    def test_should_never_have_a_negative_quality(self):
+        items = makeItemList("foo", 4, 0)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 0
 
-    def test_quality_goes_up_by_two_for_improving_products_with_10_days_or_less_left(self):
-        self.items.append(Item("Aged Brie", 10, 34))
-        self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 8, 30))
-        gilded_rose.update_quality(self.items)
-        expected = [
-            {'sell_in': 9, 'quality': 36},
-            {'sell_in': 7, 'quality': 32},
-        ]
+    def test_should_increase_the_quality_of_aged_brie(self):
+        items = makeItemList("Aged Brie", 4, 5)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 6
 
-        for index, expectation in enumerate(expected):
-            item = self.items[index]
-            assert item.quality == expectation['quality']
-            assert item.sell_in == expectation['sell_in']
+    def test_should_increase_quality_of_aged_brie_twice_as_fast_after_sellin(self):
+        items = makeItemList("Aged Brie", 1, 10)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 11
+        gilded_rose.update_quality()
+        assert items[0].quality == 13
 
-    def test_quality_goes_up_by_three_for_improving_products_with_5_days_or_less_left(self):
-        self.items.append(Item("Aged Brie", 4, 11))
-        self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 5, 15))
-        gilded_rose.update_quality(self.items)
-        expected = [
-            {'sell_in': 3, 'quality': 14},
-            {'sell_in': 4, 'quality': 18},
-        ]
+    def test_should_never_have_a_quality_greater_than_50(self):
+        items = makeItemList("Aged Brie", 0, 50)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 50
 
-        for index, expectation in enumerate(expected):
-            item = self.items[index]
-            assert item.quality == expectation['quality']
-            assert item.sell_in == expectation['sell_in']
+    def test_should_not_degrade_the_quality_of_sulfuras(self):
+        items = makeItemList("Sulfuras, Hand of Ragnaros", 0, 20)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 20
 
-    def test_quality_and_sellin_decrease_twice_as_fast_after_sell_by(self):
-        self.items.append(Item("+5 Dexterity Vest", 0, 20))
-        self.items.append(Item("Conjured Mana Cake", 0, 6))
-        gilded_rose.update_quality(self.items)
-        expected = [
-            {'sell_in': -1, 'quality': 18},
-            {'sell_in': -1, 'quality': 4},
-        ]
+    def test_should_not_change_the_sellin_of_sulfuras(self):
+        items = makeItemList("Sulfuras, Hand of Ragnaros", 10, 0)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].sell_in == 10
 
-        for index, expectation in enumerate(expected):
-            item = self.items[index]
-            assert item.quality == expectation['quality']
-            assert item.sell_in == expectation['sell_in']
+    def test_should_not_change_the_quality_of_sulfuras_even_if_created_with_sellin_passed(self):
+        items = makeItemList("Sulfuras, Hand of Ragnaros", -1, 10)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 10
 
-    def test_backstage_passes_and_brie_go_to_quality_zero_after_sell_by(self):
-        self.items.append(Item("Aged Brie", 0, 20))
-        self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 0, 20))
-        gilded_rose.update_quality(self.items)
-        expected = [
-            {'sell_in': -1, 'quality': 0},
-            {'sell_in': -1, 'quality': 0},
-        ]
+    def test_should_increase_quality_of_backstage_pass_twice_as_fast_when_less_than_10_days_remain(self):
+        items = makeItemList("Backstage passes to a TAFKAL80ETC concert", 11, 20)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 21
+        gilded_rose.update_quality()
+        assert items[0].quality == 23
+        gilded_rose.update_quality()
+        assert items[0].quality == 25
 
-        for index, expectation in enumerate(expected):
-            item = self.items[index]
-            assert item.quality == expectation['quality']
-            assert item.sell_in == expectation['sell_in']
+    def test_should_increase_quality_of_backstage_pass_three_times_as_fast_when_5_days_left(self):
+        items = makeItemList("Backstage passes to a TAFKAL80ETC concert", 6, 20)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 22
+        gilded_rose.update_quality()
+        assert items[0].quality == 25
+        gilded_rose.update_quality()
+        assert items[0].quality == 28
 
-    def test_sulfuras_the_immutable(self):
-        self.items.append(Item("Sulfuras, Hand of Ragnaros", 0, 80))
-        gilded_rose.update_quality(self.items)
-        expected = {'sell_in': 0, 'quality': 80}
-        item = self.items[0]
-        assert item.quality == expected['quality']
-        assert item.sell_in == expected['sell_in']
+    def test_should_never_increase_quality_of_backstage_pass_past_maximum_even_if_close_to_concert(self):
+        items = makeItemList("Backstage passes to a TAFKAL80ETC concert", 5, 50)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 50
 
-    def test_quality_does_not_increase_past_50(self):
-        self.items.append(Item("Aged Brie", 4, 49))
-        gilded_rose.update_quality(self.items)
-        expected = {'sell_in': 3, 'quality': 50}
-        item = self.items[0]
-        assert item.quality == expected['quality']
-        assert item.sell_in == expected['sell_in']
+    def test_should_set_quality_to_zero_for_backstagepass_after_expires(self):
+        items = makeItemList("Backstage passes to a TAFKAL80ETC concert", 1, 10)
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        assert items[0].quality == 13
+        gilded_rose.update_quality()
+        assert items[0].quality == 0
+        gilded_rose.update_quality()
+        assert items[0].quality == 0
 
-    # @skip
     # def test_conjured_items_decrease_in_quality_twice_as_fast(self):
-    #     self.items.append(Item("Conjured Mana Cake", 3, 6))
-    #     gilded_rose.update_quality(self.items)
-    #     expected = {'sell_in': 2, 'quality': 2}
-    #     item = self.items[0]
-    #     self.assertEqual(item.quality, expected['quality'])
-    #     self.assertEqual(item.sell_in, expected['sell_in'])
+    #     items = makeItemList("Conjured Mana Cake", 3, 10)
+    #     gilded_rose = GildedRose(items)
+    #     gilded_rose.update_quality()
+    #
+    #     assert items[0].quality == 8
+
