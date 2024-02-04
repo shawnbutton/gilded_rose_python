c62d9fcec9a88a6825d63ebcda0f5eefd6ad6c2b
shawn <shawnbutton@gmail.com>
2024-02-04 10:17:03 -0500

    move logic onto item

--- gilded_rose_python/gilded_rose.py
+++ gilded_rose_python/gilded_rose.py
@@ -1,7 +1,3 @@
-SULFURAS = "Sulfuras, Hand of Ragnaros"
-CONCERT_TICKETS = "Backstage passes to a TAFKAL80ETC concert"
-BRIE = "Aged Brie"
-
 
 class GildedRose(object):
 
@@ -10,54 +6,6 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            self.process_item(item)
+            item.process_item()
 
-    def process_item(self, item):
-        if self.is_legendary(item):
-            pass
-        elif self.is_cheese(item):
-            self.raise_quality(item)
-            self.lower_sellin(item)
-            if self.not_expired(item):
-                self.raise_quality(item)
-        elif self.is_tickets(item):
-            self.raise_quality(item)
-            if item.sell_in < 11:
-                self.raise_quality(item)
-            if item.sell_in < 6:
-                self.raise_quality(item)
-            self.lower_sellin(item)
-            if self.not_expired(item):
-                item.quality = 0
-        else:
-            if self.not_worthless(item):
-                self.lower_quality(item)
-            self.lower_sellin(item)
-            if self.not_expired(item):
-                if self.not_worthless(item):
-                    self.lower_quality(item)
 
-    def not_expired(self, item):
-        return item.sell_in < 0
-
-    def not_worthless(self, item):
-        return item.quality > 0
-
-    def is_legendary(self, item):
-        return item.name == SULFURAS
-
-    def is_tickets(self, item):
-        return item.name == CONCERT_TICKETS
-
-    def is_cheese(self, item):
-        return item.name == BRIE
-
-    def lower_sellin(self, item):
-        item.sell_in = item.sell_in - 1
-
-    def raise_quality(self, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-
-    def lower_quality(self, item):
-        item.quality = item.quality - 1
--- gilded_rose_python/item.py
+++ gilded_rose_python/item.py
@@ -1,5 +1,59 @@
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+CONCERT_TICKETS = "Backstage passes to a TAFKAL80ETC concert"
+BRIE = "Aged Brie"
+
 class Item:
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
+
+    def process_item(self):
+        if self.is_legendary():
+            pass
+        elif self.is_cheese():
+            self.raise_quality()
+            self.lower_sellin()
+            if self.not_expired():
+                self.raise_quality()
+        elif self.is_tickets():
+            self.raise_quality()
+            if self.sell_in < 11:
+                self.raise_quality()
+            if self.sell_in < 6:
+                self.raise_quality()
+            self.lower_sellin()
+            if self.not_expired():
+                self.quality = 0
+        else:
+            if self.not_worthless():
+                self.lower_quality()
+            self.lower_sellin()
+            if self.not_expired():
+                if self.not_worthless():
+                    self.lower_quality()
+
+    def not_expired(self):
+        return self.sell_in < 0
+
+    def not_worthless(self):
+        return self.quality > 0
+
+    def is_legendary(self):
+        return self.name == SULFURAS
+
+    def is_tickets(self):
+        return self.name == CONCERT_TICKETS
+
+    def is_cheese(self):
+        return self.name == BRIE
+
+    def lower_sellin(self):
+        self.sell_in = self.sell_in - 1
+
+    def raise_quality(self):
+        if self.quality < 50:
+            self.quality = self.quality + 1
+
+    def lower_quality(self):
+        self.quality = self.quality - 1
