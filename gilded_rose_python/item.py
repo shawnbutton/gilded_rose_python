e1a7a36e9c8866dde0487f921a0e51776d4243b3
shawn <shawnbutton@gmail.com>
2024-02-06 10:41:33 -0500

    add snapshot tests

--- gilded_rose_python/item.py
+++ gilded_rose_python/item.py
@@ -3,3 +3,6 @@ class Item:
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
+
+    def __str__(self):
+        return f'name:{self.name}, sell_in:{self.sell_in}, quality:{self.quality}'
--- tests/test_snapshot.py
+++ tests/test_snapshot.py
@@ -1,3 +1,42 @@
-def test_simple():
-    result = "Hello ApprovalTests"
-    # verify(result)
+import pathlib
+
+from gilded_rose_python.gilded_rose import GildedRose
+from gilded_rose_python.item import Item
+
+ITEM_NAMES = [
+    '+5 Dexterity Vest',
+    'Aged Brie',
+    'Backstage passes to a TAFKAL80ETC concert',
+    'Arbitrary Item'
+]
+
+
+def test_gilded_rose_with_snapshot(snapshot):
+    items = create_items()
+
+    snapshot_text = ''
+
+    gilded_rose = GildedRose(items)
+    for runs in range(0, 4):
+        snapshot_text += 'Run ' + str(runs) + '\n'
+        gilded_rose.update_quality()
+        snapshot_text += items_into_str(items)
+
+    snapshot.snapshot_dir = str(pathlib.Path(__file__).parent.resolve()) + '/snapshots'
+    snapshot.assert_match(snapshot_text, 'snapshot.txt')
+
+
+def items_into_str(items):
+    snapshot_text = ''
+    for item in items:
+        snapshot_text += str(item) + '\n'
+    return snapshot_text
+
+
+def create_items():
+    items = []
+    for name in ITEM_NAMES:
+        for sell_in in range(-1, 20):
+            for quality in range(-1, 55):
+                items.append(Item(name, sell_in, quality))
+    return items
